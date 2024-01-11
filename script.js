8a1c8f2bee801ab99b430a6ad6f0ae75a8cdfaf5
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-11 11:38:38 -0300

    melhoria no cod

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 const CloseModal = function () {
   modal.classList.add('hidden');
