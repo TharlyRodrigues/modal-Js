896ce54de2d54bb8f7331dcfab293d64becd7bbd
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-11 11:33:47 -0300

    modal

---
+++ script.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+const CloseModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', CloseModal);
+
+overlay.addEventListener('click', CloseModal);
