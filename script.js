7cbaeedb483353cf5ba0dfe9e02a1727837fe2f4
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-11 18:51:22 -0300

    add função de fecha modal com Esc

--- script.js
+++ script.js
@@ -11,10 +11,18 @@ const openModal = function () {
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
-const CloseModal = function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-btnCloseModal.addEventListener('click', CloseModal);
-overlay.addEventListener('click', CloseModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
